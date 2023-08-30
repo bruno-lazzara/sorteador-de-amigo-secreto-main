b2a59202db1512c3c59d353dda36a1b8bb5be08b
Bruno Lazzara Queiroz <bruno.queiroz@acpgroup.com.br>
2023-08-30 11:43:44 -0300

    Initial commit

---
+++ src/pages/Sorteio.test.tsx
@@ -0,0 +1,61 @@
+import { fireEvent, render, screen } from "@testing-library/react"
+import { RecoilRoot } from "recoil"
+import { useListaDeParticipantes } from "../state/hooks/useListaDeParticipantes"
+import Sorteio from "./Sorteio"
+import { useResultadoSorteio } from "../state/hooks/useResultadoSorteio"
+
+jest.mock('../state/hooks/useListaDeParticipantes', () => {
+    return {
+        useListaDeParticipantes: jest.fn()
+    }
+})
+jest.mock('../state/hooks/useResultadoSorteio', () => {
+    return {
+        useResultadoSorteio: jest.fn()
+    }
+})
+
+describe('na página de sorteio', () => {
+    const participantes = ['Bruno', 'Marcelle', 'Leo']
+    const resultado = new Map([
+        ['Bruno', 'Marcelle'],
+        ['Marcelle', 'Leo'],
+        ['Leo', 'Bruno']
+    ])
+    beforeEach(() => {
+        (useListaDeParticipantes as jest.Mock).mockReturnValue(participantes);
+        (useResultadoSorteio as jest.Mock).mockReturnValue(resultado);
+    })
+    test('todos os participantes podem exibir o seu amigo secreto', () => {
+        render(
+            <RecoilRoot>
+                <Sorteio />
+            </RecoilRoot>
+        )
+
+        const opcoes = screen.queryAllByRole('option')
+        expect(opcoes).toHaveLength(participantes.length + 1)
+    })
+    test('o amigo secreto é exibido quando solicitado', () => {
+        render(
+            <RecoilRoot>
+                <Sorteio />
+            </RecoilRoot>
+        )
+
+        const select = screen.getByPlaceholderText('Selecione o seu nome')
+
+        fireEvent.change(select, {
+            terget: {
+                value: participantes[1]
+            }
+        })
+
+        const botao = screen.getByRole('button')
+        fireEvent.click(botao)
+
+        const amigoSecreto = screen.getByRole('alert')
+
+        expect(amigoSecreto).toBeInTheDocument()
+    })
+})
